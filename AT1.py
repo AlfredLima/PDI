03f6ed33ec9b26ee4ac07461dd3bdfc44dcfe6f6
alfredlima <alfredolms@ic.ufal.br>
2018-11-15 09:37:09 -0300

    Atividade 1 de PDI

---
+++ AT1.py
@@ -0,0 +1,23 @@
+import cv2
+
+webcam = cv2.VideoCapture(0)  #instancia o uso da webcam
+count = 1
+
+while True:
+	s, imagem = webcam.read() #pega efeticamente a imagem da webcam
+	imagem = cv2.flip(imagem,180) #espelha a imagem
+
+	# Desenha um retângulo nas faces detectadas
+	cv2.imshow('Video', imagem) #mostra a imagem captura na janela
+
+	#o trecho seguinte é apenas para parar o código e fechar a janela
+	t = cv2.waitKey(1) 
+	if t == ord('q'):
+		break
+	elif t == ord('s'):
+		cv2.imwrite( "PDI_" + str(count) + ".jpg", imagem );
+		count = count + 1
+		print('Salvando')
+
+webcam.release() #dispensa o uso da webcam
+cv2.destroyAllWindows() #fecha todas a janelas abertas
