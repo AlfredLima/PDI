81cc294287efbf6cee370749e4b2089776903468
Alfredo Lima <alfredolms@ic.ufal.br>
2018-12-04 22:43:58 -0300

    Tentando fazer a rotação

--- aula3.py
+++ aula3.py
@@ -1,17 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This is a temporary script file.
-"""
-
-#%%
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import os,sys
 
-
-#%%
 def sumImg(img1, img2, a, b):    
     img1 = img1.astype(np.float64) 
     img2 = img2.astype(np.float64)
@@ -53,12 +46,43 @@ def stretchImg(img):
                 new_image[i][j][k] = abs(img1[i][j][k]-img2[i][j][k])
     return new_image
 
-#%%%
-cv2.__version__
+def norma(x, min_x, max_x):
+    return (x-min_x)/(max_x-min_x)
+
+def getRMat((cx, cy), angle, scale):
+    a = scale*m.cos(angle*np.pi/180)
+    b = scale*(m.sin(angle*np.pi/180))
+    u = (1-a)*cx-b*cy
+    v = b*cx+(1-a)*cy
+    return np.array([[a,b,u], [-b,a,v]]) 
 
 
-img1 = cv2.imread('/home/alunoic/lena.png')
-img2 = cv2.imread('/home/alunoic/baboon.png')
+def rotationImg(img, tetha):
+    sizeH, sizeV, dim = img1.shape
+    new_image = np.zeros([sizeH, sizeV, dim],dtype=np.float64)
+    img = img.astype(np.float64) 
+    
+    co = np.cos( tetha * np.pi/180 )
+    si = np.sin( tetha * np.pi/180 )
+    matrix_rotation = np.matrix( [ [co, -si, 0] , [si, co, 0], [0, 0, 1] ] )
+
+    for i in range(sizeH):
+        for j in range(sizeV):
+            new_image[i][j] = np.matmul(matrix_rotation, img[i][j])
+
+    max_x, min_x = np.max(new_image), np.min(new_image)
+    
+    for i in range(sizeH):
+        for j in range(sizeV):
+            new_image[i][j] = list(map(lambda x: x(new_image[i][j], min_x, max_x), [norma]))[0]
+
+    new_image = (255 * new_image).astype(np.uint8)
+    #print(new_image)
+    return new_image
+
+
+img1 = cv2.imread('/home/alfredo/Workspace/PDI/AT2/lena.png')
+img2 = cv2.imread('/home/alfredo/Workspace/PDI/AT2/baboon.png')
 
 cv2.imshow('img1',img1)
 cv2.waitKey(0)
@@ -71,20 +95,19 @@ sizeh2, sizev2, dim2 = img2.shape
 
 if sizeh1 == sizeh2 and sizev1 == sizev2 and dim1 == dim2:
     print('Mesmo tamanho')
-    img3 = sumImg(img1,img2,0.5,0.5)
-    cv2.imshow('img3',img3)
-    cv2.waitKey(0)
-    img4 = maxImg(img1,img2)
-    cv2.imshow('img4',img4)
-    cv2.waitKey(0)   
-    img5 = maxImg(img1,img2)
-    cv2.imshow('img5',img5)
+    #img3 = sumImg(img1,img2,0.1,0.1)
+    #cv2.imshow('img3',img3)
+    #cv2.waitKey(0)
+    #img4 = maxImg(img1,img2)
+    #cv2.imshow('img4',img4)
+    #cv2.waitKey(0)   
+    #img5 = diffAbsImg(img1,img2)
+    #cv2.imshow('img5',img5)
+    #cv2.waitKey(0)
+    img6 = rotationImg(img1,90)
+    cv2.imshow('img6',img6)
     cv2.waitKey(0)
 else:
     print('Diferente')
-
-#img3 = np.zeros([SIZE,SIZE,3],dtype=np.uint8)
-
-
 
 cv2.destroyAllWindows()
